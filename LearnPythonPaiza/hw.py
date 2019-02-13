c4ba058527ca72277fdf488c5ee744dff8cbd16f
imoto <imoto0417@mac.com>
2019-02-14 00:55:22 +0900

    add bluebacks

--- LearnPythonPaiza/hw.py
+++ LearnPythonPaiza/hw.py
@@ -4,10 +4,12 @@
 import numpy as np
 import tensorflow as tf
 
-hello = tf.constant('Hello, TensorFlow!')
+hello = tf.constant("Hello, TensorFlow!")
 sess = tf.Session()
 print(sess.run(hello))
 
 a = tf.constant(10)
 b = tf.constant(20)
 print(sess.run(a+b))
+
+print("Hello, TensorFlow!")
