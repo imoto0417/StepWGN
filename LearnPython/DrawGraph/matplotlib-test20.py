c198e0f3e996ce6fc8fcf86a8d8b617cf92ffa2a
imoto <imoto0417@mac.com>
2019-02-21 00:07:59 +0900

    numpy配列学習

--- LearnPython/DrawGraph/matplotlib-test20.py
+++ LearnPython/DrawGraph/matplotlib-test20.py
@@ -6,16 +6,19 @@ igfont = {'family':'IPAexGothic'}
 pi = math.pi   #mathモジュールのπを利用
 
 x = np.linspace(0, 360, 100)  #0から360度までの範囲を100分割したnumpy配列
-y = np.sin(x*pi/180)
+y = (np.sin(x*pi/180), np.cos(x*pi/180))
+
+lbl = ['Sin','Cos']
 
 #グラフタイトル
-plt.title('Sin',**igfont, fontsize=16)
+plt.title('Sin/Cos',**igfont, fontsize=16)
 #グラフの軸
 plt.xlabel('Angle',**igfont,fontsize=16)
 plt.ylabel('値',**igfont, fontsize=16)
 
-plt.plot(x, y, label='Sin')
+for i in range(0,2,1):
+    plt.plot(x, y[i], label=lbl[i])
 
 #グラフの凡例
-plt.legend()
+#plt.legend()
 plt.show()
