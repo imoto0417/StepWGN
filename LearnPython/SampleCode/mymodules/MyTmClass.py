d262a57712d706a3bfff225e1e5fbf6c5f9b87af
imoto <imoto0417@mac.com>
2019-03-01 00:46:31 +0900

    TF

--- LearnPython/SampleCode/mymodules/MyTmClass.py
+++ LearnPython/SampleCode/mymodules/MyTmClass.py
@@ -1,23 +1,24 @@
+# -*- coding: utf-8 -*-
+"""My Timer Module"""
+
 from datetime import datetime
 
 
 class TMClass:
-    def __init__(self, code, name):
+    """My Timer Class"""
+
+    def __init__(self):
         self.pt = datetime.today()
 
     def __del__(self):
         print("del")
 
-    def get_today_1(self):
+    def get_today_time(self):
+        """get time fuunction"""
         today = datetime.today()
         value = (today.year, today.month, today.day,
                  today.hour, today.minute, today.second, today.microsecond)
         return(value)
-
-    def get_today_2(self):
-        today = datetime.today()
-        value = (today)
-        return (self.code, self.name, self.count, str(value))
 
     def push_time(self):
         self.pt = datetime.today()
