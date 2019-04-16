eaa5c362fbcb159b63ed4751add7cfe488e183b1
Takuya Imoto <imoto0417@mac.com>
2019-04-16 22:13:12 +0900

    20190416

---
+++ FirstTENSORFLOW/load_mnist.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import tensorflow as tf
+import tflearn
+import tflearn.datasets.mnist as mnist
+from matplotlib import pyplot as plt
+from matplotlib import cm
+import numpy as np
+
+trainX, trainY, testX, testY, = mnist.load_data('./data/mnist/', one_hot=True)
+
+print(len(trainX), len(trainY))
+
+print(trainX)
+
+print(trainY)
+
+print(trainX[0], trainY[0])
+
+plt.imshow(trainX[0].reshape(28, 28), cmap=cm.gray_r, interpolation='nearest')
+plt.show()
