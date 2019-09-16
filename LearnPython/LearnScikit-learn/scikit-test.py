cceaf585fd9306dd6a2903a6a7df2421a34c58f2
Takuya Imoto <imoto0417@mac.com>
2019-09-16 20:13:44 +0900

    20190316a

--- LearnPython/LearnScikit-learn/scikit-test.py
+++ LearnPython/LearnScikit-learn/scikit-test.py
@@ -2,9 +2,17 @@
 import matplotlib.pyplot as plt
 from sklearn import datasets
 
+# cmap
+Cmaps = ["Greys", "Purples", "Blues", "Greens",
+         "Oranges", "Reds", "YlOrBr", "YlOrRd", "OrRd", "PuRd"]
+
 # 手書き文字セットを読み込む
 digits = datasets.load_digits()
 
+fig = plt.figure()
+
 # どのようなデータか、確認してみる
-plt.matshow(digits.images[9], cmap="Greys")
+for i in {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}:
+    ax = fig.add_subplot(3, 4, i + 1)
+    ax.matshow(digits.images[i], cmap=Cmaps[i])
 plt.show()
