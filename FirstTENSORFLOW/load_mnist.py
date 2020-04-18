d313c3a5324dae0a581236ff96732366398950cb
Takuya Imoto <imoto0417@mac.com>
2020-04-18 17:31:06 +0900

    20200418

--- FirstTENSORFLOW/load_mnist.py
+++ FirstTENSORFLOW/load_mnist.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import warnings
 import tensorflow as tf
 import tflearn
 import tflearn.datasets.mnist as mnist
@@ -7,15 +7,18 @@ from matplotlib import pyplot as plt
 from matplotlib import cm
 import numpy as np
 
+warnings.filterwarnings('ignore')
+
+
 trainX, trainY, testX, testY, = mnist.load_data('./data/mnist/', one_hot=True)
 
-print(len(trainX), len(trainY))
+# print(len(trainX), len(trainY))
 
-print(trainX)
+# print(trainX)
 
-print(trainY)
+# print(trainY)
 
-print(trainX[0], trainY[0])
+# print(trainX[0], trainY[0])
 
-plt.imshow(trainX[0].reshape(28, 28), cmap=cm.gray_r, interpolation='nearest')
+plt.imshow(trainX[1].reshape(28, 28), cmap=cm.gray_r, interpolation='nearest')
 plt.show()
