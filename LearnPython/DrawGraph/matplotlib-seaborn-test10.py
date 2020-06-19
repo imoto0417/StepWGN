5bcd9fe32037694f461a709964c3979f20163dc2
Takuya Imoto <ioto0417@mac.com>
2020-06-19 22:24:33 +0900

    lll

--- LearnPython/DrawGraph/matplotlib-seaborn-test10.py
+++ LearnPython/DrawGraph/matplotlib-seaborn-test10.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import numpy as np
 
-sns.set()
-sns.set_style('whitegrid')
-sns.set_palette('gray')
+#sns.set()
+#sns.set_style('whitegrid')
+#sns.set_palette('gray')
 
 x = np.array(['ERS1', 'ERS2', 'ETR1', 'ETR2', 'EIN4'])
 y = np.array([12.0, 3.1, 11.8, 2.9, 6.2])
